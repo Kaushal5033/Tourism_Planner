e7e694c495217e784e88a562ec0b323aa266240a
Kaushal5033 <kaushalrai8076958922@gmail.com>
2024-03-21 00:50:29 +0530

    finish

--- plan/src/Component/Tours.jsx
+++ plan/src/Component/Tours.jsx
@@ -10,7 +10,7 @@ const Tours = (props) => {
     return (
         <div className='container'>
             <div>
-                <h2 className='title'>Plan with Love</h2>
+                <h2 className='title'>Create Your Plan</h2>
             </div>
             <div className='cards'>
                 {
